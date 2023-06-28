0d4457fe8e5b2e3d0ea02cf47e66625a51875993
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2023-06-28 15:45:09 +0200

    Update AccessibilityModule.java

--- src/main/java/org/matsim/accessibillityDrtOptimizer/run/modules/AccessibilityModule.java
+++ src/main/java/org/matsim/accessibillityDrtOptimizer/run/modules/AccessibilityModule.java
@@ -14,7 +14,6 @@ import org.matsim.contrib.drt.schedule.DrtTaskFactory;
 import org.matsim.contrib.dvrp.fleet.Fleet;
 import org.matsim.contrib.dvrp.run.AbstractDvrpModeQSimModule;
 import org.matsim.contrib.dvrp.schedule.ScheduleTimingUpdater;
-import org.matsim.contrib.zone.skims.TravelTimeMatrix;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.mobsim.framework.MobsimTimer;
 import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
